718aee62e0617ca33c0e7219c020cc25f5118d2b
GoForMarty <marty.stepien@gmail.com>
2018-08-23 20:43:04 +0200

    refactored

--- DecimalComparator/src/com/marty/Main.java
+++ DecimalComparator/src/com/marty/Main.java
@@ -7,10 +7,10 @@ public class Main {
     }
 
     public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
-        num1 = num1 * 1000;
-        num2 = num2 * 1000;
+        num1 = (int) (num1 * 1000);
+        num2 = (int) (num2 * 1000);
 
-        if ((int) num1 == (int) num2) {
+        if (num1 == num2) {
             return true;
         }
         return false;
