d5ebf4d94d1b4fc91e336d1a8882bb9d3d853e7b
GoForMarty <marty.stepien@gmail.com>
2018-08-27 11:27:36 +0200

    day of the week challenge

---
+++ DayOfTheWeek/src/com/marty/Main.java
@@ -0,0 +1,37 @@
+package com.marty;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printDayOfTheWeek(3);
+        printDayOfTheWeek(9);
+    }
+    private static void printDayOfTheWeek(int dayNumber) {
+        switch(dayNumber) {
+            case 0:
+                System.out.println("It's Sunday");
+                break;
+            case 1:
+                System.out.println("It's Monday");
+                break;
+            case 2:
+                System.out.println("It's Tuesday");
+                break;
+            case 3:
+                System.out.println("It's Wednesday");
+                break;
+            case 4:
+                System.out.println("It's Thursday");
+                break;
+            case 5:
+                System.out.println("It's Friday");
+                break;
+            case 6:
+                System.out.println("It's Saturday");
+                break;
+
+            default:
+                System.out.println("There's only 0-6 available days in a week");
+        }
+    }
+}
