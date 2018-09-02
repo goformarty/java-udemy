0fd3eb999ff98c1942cdbc026541c1f9e5b657ca
GoForMarty <marty.stepien@gmail.com>
2018-09-02 13:44:18 +0200

    solution 2

--- LargestPrime/src/com/marty/Main.java
+++ LargestPrime/src/com/marty/Main.java
@@ -4,10 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(isPrime(31));
+
         System.out.println(getLargestPrime(3));
         System.out.println(getLargestPrime(17));
         System.out.println(getLargestPrime(22));
+        
+        System.out.println(getLargestPrime2(8));
+        System.out.println(getLargestPrime2(22));
+
     }
+//    ***************************
+//    SOLUTION 1
+//    ***************************
 
     private static int getLargestPrime(int num) {
 
@@ -27,5 +35,21 @@ public class Main {
         }
 
         return true;
+    }
+
+//    ***************************
+//    SOLUTION 2
+//    ***************************
+
+    private static int getLargestPrime2(int num) {
+
+        if (num < 2) return -1;
+
+        for (var i = 2; i < num; i++) {
+            while (num % i == 0 && num != i) {
+                num /= i;
+            }
+        }
+        return num;
     }
 }
