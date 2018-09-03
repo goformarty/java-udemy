9a7adda387d0c7016a6689511827c66a88b83043
GoForMarty <marty.stepien@gmail.com>
2018-09-03 13:22:34 +0200

    handling non integers and date range

--- ReadingUserInput/src/com/marty/Main.java
+++ ReadingUserInput/src/com/marty/Main.java
@@ -9,16 +9,31 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your year of birth: ");
+        boolean hasNextInt = scanner.hasNextInt();
+
+        if(hasNextInt) {
+
         int yearOfBirth = scanner.nextInt();
         scanner.nextLine(); // handle next line character - enter
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
 
-        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-        int age = currentYear - yearOfBirth;
-        System.out.println("your name is " + name + " and you're " + age + " years old." );
+            int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+            int age = currentYear - yearOfBirth;
 
+            if (age >= 0 && age <= 125) {
+                System.out.println("your name is " + name + " and you're " + age + " years old.");
+            } else {
+                System.out.println("Invalid age");
+            }
+
+        } else {
+
+            System.out.println("It's not a number");
+
+        }
+        
         scanner.close();
     }
 }
