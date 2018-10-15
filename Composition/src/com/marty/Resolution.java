bbdb3b106d86edd8c8c1d645942a5ff325e7ba46
Marty <marty.stepien@gmail.com>
2018-10-15 16:25:50 +0100

    compoisition start

---
+++ Composition/src/com/marty/Resolution.java
@@ -0,0 +1,19 @@
+package com.marty;
+
+public class Resolution {
+    int width;
+    int height;
+
+    public Resolution(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
---
+++ Composition/src/com/marty/Vehicle.java
@@ -0,0 +1,9 @@
+package com.marty;
+
+public class Vehicle {
+    private String name;
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+}
