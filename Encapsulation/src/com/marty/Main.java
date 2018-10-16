aacf4fe4d7e745239c58453fab35af6ddbdcb29d
Marty <marty.stepien@gmail.com>
2018-10-16 14:42:33 +0100

    enhanced class player with encapsulation added

--- Encapsulation/src/com/marty/Main.java
+++ Encapsulation/src/com/marty/Main.java
@@ -5,10 +5,14 @@ public class Main {
     public static void main(String[] args) {
 	    Player player = new Player();
 	    player.name = "Marty";
-	    player.health = 20;
+	    // player.health = 20;
 	    player.weapon = "claws";
+		System.out.println("Remaining health = " + player.healthRemaining());
 
-	    player.loseHealth(10);
-        System.out.println("Remaining health = " + player.healthRemaining());
+		player.loseHealth(10);
+		System.out.println("Remaining health = " + player.healthRemaining());
+
+		EnhancedPlayer enhancedPlayer = new EnhancedPlayer("Super Marty", 50, "Sword");
+		System.out.println("Initial health is " + enhancedPlayer.getHealth());
     }
 }
